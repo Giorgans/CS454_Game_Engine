41e73bab97875ee4854de17a38edf35571b84332
Panagiotis Papadakos <papadako@csd.uoc.gr>
2021-11-16 11:49:00 +0200

    test

---
+++ cmake-build-debug/CMakeFiles/CS454_Super_Mario_Game.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS454_Super_Mario_Game.dir/main.cpp.o"
+  "CS454_Super_Mario_Game"
+  "CS454_Super_Mario_Game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS454_Super_Mario_Game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

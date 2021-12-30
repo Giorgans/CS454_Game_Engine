14cdd3d618b59476197c1e42a32a90016d6dcfcb
Ιάσων Φίλιππος Νταγιάννης <csd3540@csd.uoc.gr>
2021-12-30 15:26:03 +0200

    primitive quit

--- Engine/Src/input.cpp
+++ Engine/Src/input.cpp
@@ -3,44 +3,75 @@
 //
 
 #include "../Include/input.h"
-#include "../Include/rendering.h"
-extern ALLEGRO_DISPLAY window;
+
+extern ALLEGRO_DISPLAY *window;
 ALLEGRO_EVENT_QUEUE *event_queue;
 ALLEGRO_EVENT event;
-void input(void){
+
+void input(){
     event_queue = al_create_event_queue();
     al_register_event_source(event_queue, al_get_keyboard_event_source());
+    al_register_event_source(event_queue, al_get_display_event_source(window));
+
     while (true) {
         al_wait_for_event(event_queue, &event);
         if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
-            assert(0);
+            /*TODO
+             * replace assert(0) with a smoother quit without errors*/
+            //done=true;
+           assert(0);
+
         }
-        if (event.type == ALLEGRO_EVENT_KEY_CHAR) {
+       else if (event.type == ALLEGRO_EVENT_KEY_CHAR) {
+
             if (event.keyboard.keycode == ALLEGRO_KEY_RIGHT) {
                 background->Scroll(16, 0);
                 terrain->Scroll(16, 0);
                 break;
             }
-            if (event.keyboard.keycode == ALLEGRO_KEY_LEFT) {
+            else if (event.keyboard.keycode == ALLEGRO_KEY_LEFT) {
                 background->Scroll(-16, 0);
                 terrain->Scroll(-16, 0);
                 break;
             }
-            if (event.keyboard.keycode == ALLEGRO_KEY_UP) {
+            else if (event.keyboard.keycode == ALLEGRO_KEY_UP) {
                 background->Scroll(0, 8);
                 terrain->Scroll(0, 8);
                 break;
             }
 
-            if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) {
+           else if (event.keyboard.keycode == ALLEGRO_KEY_DOWN) {
                 background->Scroll(0, -8);
                 terrain->Scroll(0, -8);
                 break;
             }
-            if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
+            else if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
+                /*TODO
+                    * replace assert(0) with a smoother quit without errors*/
+                //done=true;
                 assert(0);
             }
+            else if (event.keyboard.keycode == ALLEGRO_KEY_ENTER){
+                /*TODO
+                    * implement start function*/
+                //done=true;
+
+            }
+
+            else if(event.keyboard.keycode == ALLEGRO_KEY_Z){
+                /*TODO
+                 * implement jump function*/
+                std::cout<<"JUMP"<<std::endl;
+
+            }
+            else if(event.keyboard.keycode == ALLEGRO_KEY_X){
+                /*TODO
+                 * implement run function*/
+                std::cout<<"RUN"<<std::endl;
+
+            }
+
         }
-    }
+}
 
 }

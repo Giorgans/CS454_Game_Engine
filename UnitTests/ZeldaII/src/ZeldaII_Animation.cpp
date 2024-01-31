1ee4776d77af792028f89e875e30023efe98b4ad
George Zervos <g.zervos96@windowslive.com>
2024-01-31 20:30:43 +0200

    bridge and map better

--- UnitTests/ZeldaII/src/ZeldaII_Animation.cpp
+++ UnitTests/ZeldaII/src/ZeldaII_Animation.cpp
@@ -22,6 +22,8 @@ void Elevator_Animations_OnAction(Sprite *sprite, Animator *animator, const Movi
 
 void Door_Animations_OnAction(Sprite *sprite, Animator *animator, const MovingAnimation &anim);
 
+void Bridge_Animations_OnAction(Sprite *sprite, Animator *animator, const MovingAnimation &anim);
+
 void Link_Animations_OnAction(Sprite *sprite, Animator *animator, const FrameRangeAnimation &anim);
 
 void Link_Animations_OnFinish(Animator *animator);
@@ -138,6 +140,18 @@ void InitializeAnimations() {
             DoorAnimator->Start(Standing, GetGameTime());
 
         }
+        //Bridge
+        else if(i->GetTypeId() == "Bridge"){
+            auto *BridgeAnimator = new FrameRangeAnimator("BridgeAnimator");
+            BridgeAnimator->SetOnAction(
+                    [i, BridgeAnimator, Standing](Animator *animator, const Animation &anim) {
+                        Bridge_Animations_OnAction(i, BridgeAnimator, *Standing);
+                    }
+            );
+            BridgeAnimator->Start(Standing, GetGameTime());
+
+        }
+
     }
 
     //TitleScreen
@@ -166,15 +180,22 @@ void InitializeAnimations() {
 
 void Elevator_Animations_OnAction(Sprite *sprite, Animator *animator, const MovingAnimation &anim) {
     auto *ElevatorStandingAnimation = new MovingAnimation("Standing", 0, 0, 0, FRAME_DURATION);
-    auto *moveDown = new MovingAnimation("Down", 8, 0, 4, FRAME_DURATION);
+    auto *move = new MovingAnimation("Down", 8, 0, 4, FRAME_DURATION);
     auto *moveGoingDown = new MovingAnimation("Down", 28, 0, 4, FRAME_DURATION);
 
     auto *movingAnimator = (MovingAnimator *) animator;
 
     if (sprite->GetStateID() == "Down") {
         if (movingAnimator->GetAnim()->IsForever())
-            movingAnimator->SetAnim(moveDown, GetGameTime());
-        sprite->SetHasDirectMotion(true).Move(moveDown->GetDx(), moveDown->GetDy()).SetHasDirectMotion(false);
+            movingAnimator->SetAnim(move, GetGameTime());
+        sprite->SetHasDirectMotion(true).Move(move->GetDx(), move->GetDy()).SetHasDirectMotion(false);
+
+    }
+
+    if (sprite->GetStateID() == "Up" ) {
+        if (movingAnimator->GetAnim()->IsForever())
+            movingAnimator->SetAnim(move, GetGameTime());
+        sprite->SetHasDirectMotion(true).Move(move->GetDx(), -move->GetDy()).SetHasDirectMotion(false);
 
     }
 
@@ -189,10 +210,37 @@ void Elevator_Animations_OnAction(Sprite *sprite, Animator *animator, const Movi
         Rect terrainview = {terrain->GetViewWindow().x, terrain->GetViewWindow().y + 240, terrain->GetViewWindow().w,
                             terrain->GetViewWindow().h};
         terrain->SetViewWindow(terrainview);
+        terrain->GetGrid()->SetViewWindow(terrainview);
+        background->SetViewWindow(terrainview);
+    }
+
+    if (sprite->GetStateID() == "Up" && movingAnimator->GetCurrRep() == 6) {
+        Rect terrainview = {terrain->GetViewWindow().x, terrain->GetViewWindow().y - 240, terrain->GetViewWindow().w,
+                            terrain->GetViewWindow().h};
+        terrain->SetViewWindow(terrainview);
+        terrain->GetGrid()->SetViewWindow(terrainview);
         background->SetViewWindow(terrainview);
     }
 
 
+}
+
+void Bridge_Animations_OnAction(Sprite *sprite, Animator *animator, const MovingAnimation &anim){
+    auto FallAnimation = new FrameRangeAnimation("FallAnimation",0,AnimationFilmHolder::GetHolder().Load(FallingBridge)->GetTotalFrames()-1,0,0,0,1000/6);
+    auto *frameRangeAnimator = (FrameRangeAnimator *) animator;
+
+    if(sprite->GetStateID()=="Fall"){
+        frameRangeAnimator->SetAnim(FallAnimation,GetGameTime());
+        sprite->SetFrame(0);
+        sprite->SetStateID("Falling");
+    }
+    else if(sprite->GetStateID()=="Falling"){
+        sprite->SetFrame(frameRangeAnimator->GetCurrFrame());
+        if(sprite->GetFrame()==0){
+            sprite->SetVisibility(false);
+            frameRangeAnimator->Stop();
+        }
+    }
 }
 
 
@@ -585,6 +633,11 @@ void Link_Animations_OnAction(Sprite *sprite,Animator *animator,const FrameRange
                 dx = 0;
                 Player->SetStateID("");
             }
+            if(Player->GetStateID()=="OnBridge") {
+                dy = 0;
+                Player->SetStateID("");
+            }
+
             Player->SetHasDirectMotion(true).Move(dx, dy).SetHasDirectMotion(false);
             Player->SetFrame(PlayerAnimator->GetCurrFrame());
         }
@@ -602,11 +655,17 @@ void Link_Animations_OnAction(Sprite *sprite,Animator *animator,const FrameRange
 
             int dx = -PlayerAnimator->GetAnim()->GetDx();
             int dy = 4;
+
             terrain->GetGrid()->FilterGridMotion(Player->GetBox(), &dx,&dy);
             if(Player->GetStateID()=="YouShallNotPassLeft") {
                 dx = 0;
                 Player->SetStateID("");
             }
+            if(Player->GetStateID()=="OnBridge") {
+                dy = 0;
+                Player->SetStateID("");
+            }
+
 
             Player->SetHasDirectMotion(true).Move(dx, dy).SetHasDirectMotion(false);
             Player->SetFrame(PlayerAnimator->GetCurrFrame());
@@ -678,8 +737,6 @@ void Link_Animations_OnAction(Sprite *sprite,Animator *animator,const FrameRange
             isStanding= true;
         }
 
-
-
         if (isStanding) {
             if (PlayerAnimator->GetAnim() != StandingAnimation) {
                 PlayerAnimator->SetAnim(StandingAnimation, GetGameTime());
@@ -696,6 +753,10 @@ void Link_Animations_OnAction(Sprite *sprite,Animator *animator,const FrameRange
 
             int dx = 0;
             int dy = 4;
+            if(Player->GetStateID()=="OnBridge") {
+                dy = 0;
+                Player->SetStateID("");
+            }
             terrain->GetGrid()->FilterGridMotion(Player->GetBox(), &dx, &dy);
             Player->SetHasDirectMotion(true).Move(dx, dy).SetHasDirectMotion(false);
 
--- UnitTests/ZeldaII/src/ZeldaII_Initializations.cpp
+++ UnitTests/ZeldaII/src/ZeldaII_Initializations.cpp
@@ -98,7 +98,6 @@ void InitializeRendering(){
         window = al_create_display(DISPLAY_W,DISPLAY_H);
         al_set_display_icon(window, al_load_bitmap(ICON_FILE_PATH));
     }
-
     if (background == nullptr)
         background = new TileLayer(MAX_HEIGHT, MAX_WIDTH, al_load_bitmap(TILESET_FILE_PATH), BACKGROUND_CSV_FILE_PATH);
     if (terrain == nullptr)
